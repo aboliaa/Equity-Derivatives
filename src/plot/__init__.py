27f2be9ad5263d78c59b51d54721875c1a0ead47
Amit Kulkarni <amitkulkarni@Amit-Kulkarnis-MacBook-Pro.local>
2016-01-23 23:20:12 +0530

    file missing in last commit

--- src/plot/__init__.py
+++ src/plot/__init__.py
@@ -1,10 +1,11 @@
 from const import *
 import plotly
-from graph import Graph
-from table import Table
+
+#from graph import Graph
+#from table import Table
 
 class Plot(object):
     def __init__(self, path=PLOT_PATH):
-        self.graph = Graph(path)
-        self.table = Table(path)
+        #self.graph = Graph(path)
+        #self.table = Table(path)
         self.plotly = plotly
