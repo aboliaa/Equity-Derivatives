db96cce3c26d6a7a314a56ed507a1177d7bb8b73
Amit Kulkarni <amitkulkarni@Amit-Kulkarnis-MacBook-Pro.local>
2016-01-26 00:11:10 +0530

    Removing old error file

--- src/db/error.py
+++
@@ -1,9 +0,0 @@
-class DBError(Exception):
-    def __init__(self, errno, msg=''):
-        super(DBError, self).__init__(msg)
-        self.errno = errno
-        self.msg = msg
-
-EEXISTS         = 10000001
-ENOTFOUND       = 10000002
-
