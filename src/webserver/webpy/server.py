28a939747ad1647086ce974585a872b1625a503a
Amit Kulkarni <amitkulkarni@Amit-Kulkarnis-MacBook-Pro.local>
2016-02-12 23:10:27 +0530

    Allowing only one request at a time.

--- src/webserver/webpy/server.py
+++ src/webserver/webpy/server.py
@@ -1,5 +1,6 @@
 import web
 import itertools
+import threading
 
 import handlers
 
@@ -12,11 +13,32 @@ URLS = {
  
 class WServer():
     def __init__(self):
-        pass
+        self.lock = threading.Lock()
+        self.inprogress = False
+
+    def request_locked(self, handle):
+        msg = "Hold your Horses. Some other request is in progress."
+        if self.inprogress:
+            raise web.internalerror(msg)
+            # return web.webapi.InternalError(message=msg)
+
+        with self.lock: 
+            if self.inprogress:
+                raise web.internalerror(msg)
+                # return web.webapi.InternalError(message=msg)
+
+            self.inprogress = True
+
+        try:
+            return handle()
+        finally:
+            self.inprogress = False
 
     def make_app(self):
         urls = tuple(itertools.chain(*URLS.iteritems()))
         self.app = web.application(urls, globals(), autoreload=False)
+        self.app.add_processor(self.request_locked)
 
     def run_app(self):
         self.app.run()
+
